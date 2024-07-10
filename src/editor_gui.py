e2790ff5508949cdf76381f81440ccdfe45e5b31
Josh Bivens <thejoshbivens@gmail.com>
2024-07-09 21:32:38 -0600

    Implement Save/Save as functionality
    Now I need to actually write to the buffer

--- src/editor_gui.py
+++ src/editor_gui.py
@@ -21,10 +21,13 @@ class EditorGUI:
         self.root.config(menu=menu)
 
         # File
-        file_menu = tk.Menu(menu)
+        file_menu = tk.Menu(menu, tearoff=0)
         menu.add_cascade(label="File", menu=file_menu)
         file_menu.add_command(label="New", command=self.new_file)
         file_menu.add_command(label="Open", command=self.open_file)
+        file_menu.add_command(label="Save", command=self.save_file)
+        file_menu.add_command(label="Save as", command=self.save_file_as)
+        # file_menu.add_separator()
 
     def new_file(self) -> None:
         self.text_editor.text_buffer = ""
@@ -36,3 +39,16 @@ class EditorGUI:
             self.text_editor.open_file(file_path)
             self.text_area.delete("1.0", "end")
             self.text_area.insert("1.0", self.text_editor.text_buffer)
+
+    def save_file(self) -> None:
+        if self.text_editor.current_file:
+            self.text_editor.save_file_as(self.text_editor.current_file)
+            messagebox.showinfo("Info", "File saved successfully")
+        else:
+            self.save_file_as()
+
+    def save_file_as(self) -> None:
+        file_path = filedialog.asksaveasfilename()
+        if file_path:
+            self.text_editor.save_file_as(file_path)
+            messagebox.showinfo("Info", "File saved successfully")
--- src/text_editor.py
+++ src/text_editor.py
@@ -4,6 +4,13 @@ class TextEditor:
         self.text_buffer = ""
 
     def open_file(self, file_path: str) -> None:
-        with open(file_path, encoding="utf8") as file:
+        with open(file_path, "r", encoding="utf8") as file:
             self.current_file = file_path
             self.text_buffer = file.read()
+
+    def save_file_as(self, file_path: str) -> None:
+        with open(file_path, "w", encoding="utf8") as file:
+            self.current_file = file_path
+            file.write(self.text_buffer)
+
+    # TODO: Now we need to save the text_buffer to the file
