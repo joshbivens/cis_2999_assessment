96887c55530c7a7ca72210536daaa33321108bac
Josh Bivens <thejoshbivens@gmail.com>
2024-07-18 18:15:01 -0600

    Update find_replace_dialog.py
    Redo grid sizing/placement

--- src/find_replace_dialog.py
+++ src/find_replace_dialog.py
@@ -4,15 +4,17 @@ from tkinter import ttk, messagebox
 class FindReplaceDialog(tk.Toplevel):
     def __init__(self, parent, text_area):
         super().__init__(parent)
-        self.title("Find and Replace")
         self.text_area = text_area
-        self.geometry("250x100")
+        self.title("Find/Replace")
+        self.transient(parent)
         self.resizable(False, False)
+        self.protocol("WM_DELETE_WINDOW", self.destroy)
 
+        self.draw_gui()
+
+    def draw_gui(self):
         # Find
-        ttk.Label(self, text="Find:").grid(
-            row=0, column=0, sticky="w", 
-            padx=5, pady=5)
-        self.find_entry = ttk.Entry(self)
-        self.find_entry.grid(row=0, column=1, padx=5, pady=5)
+        ttk.Label(self, text="Find:").grid(row=0, column=0, sticky="e", padx=5, pady=5)
+        ttk.Entry(self, textvariable=self.find_var).grid(row=0, column=1, columnspan=2, sticky="we", padx=5, pady=5)
+        ttk.Button(self, text="Find Next", command=self.find_next).grid(row=0, column=3, sticky="w", padx=5, pady=5)
 
