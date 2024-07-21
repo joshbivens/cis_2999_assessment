bc998919ae478a88ff24c6867f2ad4375429b07e
Josh Bivens <thejoshbivens@gmail.com>
2024-07-20 21:43:16 -0600

    Update syntax_highlighted_text.py
    Ha! It works. Now to refine

--- src/syntax_highlighted_text.py
+++ src/syntax_highlighted_text.py
@@ -6,30 +6,34 @@ from pygments.styles import get_style_by_name
 class SyntaxHighlightedText(tk.Text):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, **kwargs)
-        self.configure(font=('Courier', 12))
+        self.configure(font=('Courier', 10))
 
         self.lexer = get_lexer_by_name("python")
-        self.style = get_style_by_name("default")
+        self.style = get_style_by_name("monokai")
         self.setup_tags()
 
     def setup_tags(self):
-        for token, opts in self.styles.items():
-            foreground = opts['color'] if 'color' in opts else None
-            background = opts['bgcolor'] if 'bgcolor' in opts else None
-            font = 'bold' if 'bold' in opts else 'normal'
-            
-            self.tag_configure(str(token), foreground=foreground, 
-                               background=background, font=font)
+        for token, style in self.style:
+            fg = self._format_color(style['color']) if 'color' in style else "#000000"
+            bg = self._format_color(style['bgcolor']) if 'bgcolor' in style else "#ffffff"
+            font = 'bold' if 'bold' in style and style['bold'] else 'normal'
+            self.tag_configure(str(token), foreground=fg, background=bg, font=font)
+
+    def _format_color(self, color):
+        if color and not color.startswith('#'):
+            color = f'#{color}'
+        return color
 
     def highlight(self, event=None):
-        # Get text content from the text widget
-        content = self.text_area.get("1.0", "end-1c")
+        content = self.get("1.0", "end-1c")
+        self.mark_set("range_start", "1.0")
 
-        # Set tag at beginning of text widget
-        self.text_area.mark_set("range_start", "1.0")
+        for tag in self.tag_names():
+            self.tag_remove(tag, "1.0", "end")
 
-        # For each token, apply start and end tags
         for token, content in lex(content, self.lexer):
-            self.text_area.mark_set("range_end", f"range_start + {len(content)}c")
-            self.text_area.tag_add(str(token), "range_start", "range_end")
-            self.text_area.mark_set("range_start", "range_end")
+            self.mark_set("range_end", f"range_start + {len(content)}c")
+            self.tag_add(str(token), "range_start", "range_end")
+            self.mark_set("range_start", "range_end")
+
+        self.edit_modified(False)
