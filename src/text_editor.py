2e9f3d59a6917cde936bdb67e63af882d182b3bd
Josh Bivens <thejoshbivens@gmail.com>
2024-07-22 00:32:03 -0600

    Update text_editor.py

--- src/text_editor.py
+++ src/text_editor.py
@@ -3,11 +3,13 @@ class TextEditor:
         self.current_file = None
         self.text_buffer = ""
 
+    
     def open_file(self, file_path: str) -> None:
         with open(file_path, "r", encoding="utf8") as file:
             self.current_file = file_path
             self.text_buffer = file.read()
 
+    
     def save_file_as(self, file_path: str) -> None:
         with open(file_path, "w", encoding="utf8") as file:
             self.current_file = file_path
